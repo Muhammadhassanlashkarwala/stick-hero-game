b58dcc13a19324a7674ee549c3c99ca3efb7c90e
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-08-02 04:17:35 +0500

    add

--- app.js
+++ app.js
@@ -191,8 +191,8 @@ window.addEventListener("mouseup" , (e)=>{
         lastTimestamp = timestamp;
         window.requestAnimationFrame(animate);
         return;
-    }
-  };
+    };
+ 
 
   switch (phase) {
     case "waiting":
@@ -212,8 +212,71 @@ window.addEventListener("mouseup" , (e)=>{
                     //Increase score
                     score += perfectHit ? 2 : 1 ;
                     scoreElement.innerText = score;
-                }
 
+                    if (perfectHit) {
+                      perfectElement.style.opacity = 1;
+                      setTimeout(()  => (perfectElementstyle.opacity = 0 ),1000);
+                    }
+                    generatePlatform();
+                    generateTree();
+                    generateTree();
+                }
+           phase = "walking"
             }
+            break;
               }   
-  }
+              case "walking" : {
+                heroX += (timestamp - lastTimestamp) / walkingSpeed;
+                const [nextPlatform] = thePlatformTheStickHits();
+                if (nextPlatform) {
+                  // If hero will reach another platform then limit it's position at it's edge
+                  const maxHeroX = nextPlatform.x + nextPlatform.w - heroDistanceFromEdge;
+                  if (heroX > maxHeroX) {
+                    heroX = maxHeroX;
+                    phase = "transitioning";
+                  }
+                } else{
+                    // If hero won't reach another platform them limit it's position at the end of the pole.
+                    const maxHeroX = sticks.last().x + sticks.last().length + heroWidth;
+                    if (heroX > maxHeroX) {
+                      heroX = maxHeroX;
+                      phase = "falling";
+                    }
+                  }
+                  break;
+              }
+              case "transitioning" : {
+                sceneOffset += (timestamp - lastTimestamp) / transitioningSpeed;
+                const [nextPlatform] = thePlatformTheStickHits();
+                if (sceneOffset > nextPlatform.x + nextPlatform.w - paddingX) {
+                  // Add the next step 
+                  sticks.push({
+                    x: nextPlatform.x + nextPlatform.w,
+                    length : 0,
+                    rotation : 0,
+                  });
+                  phase = "waiting"
+                }
+                break;
+              }
+              case "falling" : {
+                if (sticks.last().rotation < 180 ) 
+                   sticks.last().rotation += (timestamp - lastTimestamp) / turningSpeed;
+                    heroY += (timestamp - lastTimestamp) / fallingSpeed;
+                    const maxHeroY = platformHeight + 100 + (window.innerHeight - canvasHeight) / 2;
+                    if (heroY > maxHeroY) {
+                      restartButton.style.display = "block";
+                      return;
+                    }
+                    break;
+                }
+                 default: throw Error("Wrong phase");
+              }
+
+
+            draw();
+            window.requestAnimationFrame(animate);
+
+            lastTimestamp = timestamp ;
+          };     
+  
